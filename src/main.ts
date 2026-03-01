42efb81ba3a0f92e13d6b11b534313fb659a3bde
[luis ngel] <olaquehace568@gmail.com>
2026-03-01 14:55:12 -0600

    correcciones

--- src/main.ts
+++ src/main.ts
@@ -5,19 +5,25 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // activamos la validación global de todos los DTOs
+  app.enableCors({
+    origin: 'http://localhost:4200',
+    credentials: true,
+  });
+
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
-      // aqui se ignoran los campos que no están en el DTO
       forbidNonWhitelisted: true,
-      // esto hace que lanze error si llegan campos no permitidos
       transform: true,
-      // Convierte automáticamente tipos (ej. string "1" → number 1)
     }),
   );
 
-  await app.listen(process.env.PORT ?? 3000);
-  console.log(`🚀 Servidor corriendo en: http://localhost:${process.env.PORT ?? 3000}`);
+  const port = process.env.PORT ?? 3000;
+
+  await app.listen(port);
+
+  console.log(`🚀 Servidor corriendo en: http://localhost:${port}`);
 }
+
 bootstrap();
