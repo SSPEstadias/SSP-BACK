205ef05dbd916311d351f6a2f30a03f3dc8a074f
YahirLeonn <yahirleon.antonio@gmail.com>
2026-03-04 13:45:01 -0600

    Tablas de oficios F1 Y F2 (entity, dtos, service y controller)

--- src/modules/civico/civico.module.ts
+++ src/modules/civico/civico.module.ts
@@ -1,17 +1,25 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
-// Entidades del módulo Cívico
-import { ExpedienteCivico } from './expedientes/expediente-civico.entity';
-import { BitacoraCivica } from './bitacora/bitacora-civica.entity';
+// Entidades
+import { ExpedienteCivico }     from './expedientes/expediente-civico.entity';
+import { BitacoraCivica }       from './bitacora/bitacora-civica.entity';
+import { EntrevistaClinica }    from './f1-entrevista/entrevista-clinica.entity';
+import { EstudioSocioeconomico } from './f2-estudio/estudio-socioeconomico.entity';
 
-// Servicios y controladores
+// Servicios
 import { ExpedientesCivicoService } from './expedientes/expedientes-civico.service';
-import { ExpedientesCivicoController } from './expedientes/expedientes-civico.controller';
-import { BitacoraCivicaService } from './bitacora/bitacora-civica.service';
-import { BitacoraCivicaController } from './bitacora/bitacora-civica.controller';
+import { BitacoraCivicaService }    from './bitacora/bitacora-civica.service';
+import { F1EntrevistaService }      from './f1-entrevista/f1-entrevista.service';
+import { F2EstudioService }         from './f2-estudio/f2-estudio.service';
 
-// Módulos del núcleo compartido que necesitamos
+// Controladores
+import { ExpedientesCivicoController } from './expedientes/expedientes-civico.controller';
+import { BitacoraCivicaController }    from './bitacora/bitacora-civica.controller';
+import { F1EntrevistaController }      from './f1-entrevista/f1-entrevista.controller';
+import { F2EstudioController }         from './f2-estudio/f2-estudio.controller';
+
+// Núcleo compartido
 import { SharedModule } from '../../shared/shared.module';
 
 @Module({
@@ -19,20 +27,27 @@ import { SharedModule } from '../../shared/shared.module';
     TypeOrmModule.forFeature([
       ExpedienteCivico,
       BitacoraCivica,
+      EntrevistaClinica,
+      EstudioSocioeconomico,
     ]),
-    SharedModule, // Expone BeneficiariosService, UsersService, ActividadesService, AuthModule
+    SharedModule,
   ],
   controllers: [
     ExpedientesCivicoController,
     BitacoraCivicaController,
+    F1EntrevistaController,
+    F2EstudioController,
   ],
   providers: [
     ExpedientesCivicoService,
     BitacoraCivicaService,
+    F1EntrevistaService,
+    F2EstudioService,
   ],
   exports: [
     ExpedientesCivicoService,
-    // Exportamos el servicio por si lo necesita algún submódulo (F1, F2, F3...)
+    F1EntrevistaService, // ← F3 necesitará verificar estatus F1
+    F2EstudioService,    // ← F3 necesitará verificar estatus F2 (candado RF-008)
   ],
 })
 export class CivicoModule {}
