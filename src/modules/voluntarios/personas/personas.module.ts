dda891b4776361eb64bffa51af6209ee4fd4698e
[luis ngel] <olaquehace568@gmail.com>
2026-04-04 18:01:27 -0600

    module agregado service y controller

--- src/modules/voluntarios/personas/personas.module.ts
+++ src/modules/voluntarios/personas/personas.module.ts
@@ -4,12 +4,14 @@ import { SharedModule } from 'src/shared/shared.module';
 import { VoluntariosGoogleDriveModule } from 'src/shared/google-drive/voluntarios-google-drive.module';
 import { PersonasService } from './personas.service';
 import { PersonasController } from './personas.controller';
+import { PersonasCsvService } from './personas-csv.service';
+import { PersonasCsvController } from './personas-csv.controller';
 import { Persona } from './entities/persona.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Persona]), SharedModule, VoluntariosGoogleDriveModule],
-  controllers: [PersonasController],
-  providers: [PersonasService],
+  controllers: [PersonasController,PersonasCsvController],
+  providers: [PersonasService,PersonasCsvService],
   exports: [PersonasService],
 })
 export class PersonasModule {}
